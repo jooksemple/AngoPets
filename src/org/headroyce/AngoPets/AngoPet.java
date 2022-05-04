c6676fe6b7f93ec18170bc126effbe7867e35711
Jack Sample <jacks2025@headroyce.org>
2022-05-04 12:00:21 -0700

    Second Commit JavaFX

--- src/org/headroyce/AngoPets/AngoPet.java
+++ src/org/headroyce/AngoPets/AngoPet.java
@@ -80,7 +80,9 @@ public class AngoPet {
     }
 
     public void move() {
-        this.x +=1;
+
+            this.x += this.xSpeed;
+            this.y += this.ySpeed;
     }
 
     public void render( Canvas canvas ) {
