847a7cf57ff118e6bf373990ca1e4123f3903ede
Rogue Su-North <naomis2025@headroyce.org>
2022-05-09 15:05:08 -0700

    Rogue second Commit

--- src/org/headroyce/AngoPets/Button.java
+++ src/org/headroyce/AngoPets/Button.java
@@ -16,14 +16,9 @@ public class Button {
     private boolean isOn;
     private boolean isShowing;
 
-    public Button(double x, double y, double width, double height) {
-        isOn = false;
-        isShowing = true;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
+    /**
+     * Creates a standard button
+     */
     public Button() {
         isOn = false;
         isShowing = true;
@@ -33,22 +28,77 @@ public class Button {
         this.height = 40;
         this.text = "text";
     }
+
+    /**
+     * Creates a button with custom coordinates and dimensions
+     * @param x the x coordinate of the button
+     * @param y the y coordinate of the button
+     * @param width the width of the button
+     * @param height the height of the button
+     */
+    public Button(double x, double y, double width, double height) {
+        isOn = false;
+        isShowing = true;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Gets the status of isOn (Whether the button has been clicked)
+     * @return the current status of isOn
+     */
     public boolean isOn() { return this.isOn; }
+
+    /**
+     * Gets the X coordinate of the Button
+     * @return the X coordinate
+     */
     public double getX() {
         return this.x;
     }
+    /**
+     * Gets the Y coordinate of the Button
+     * @return the Y coordinate
+     */
     public double getY() {
         return this.y;
     }
+    /**
+     * Gets the width of the button
+     * @return the width of the button
+     */
     public double getWidth() {
         return this.width;
     }
+    /**
+     * Gets the height of the button
+     * @return the height of the button
+     */
     public double getHeight() {
         return this.height;
     }
+    /**
+     * Gets the color of the button
+     * @return the color of the button
+     */
     public Color getColor() {return this.color; }
+    /**
+     * Gets the text set to the button
+     * @return the text of the button
+     */
     public String getText() {return this.text; }
+    /**
+     * Sets the speed of the button in the X direction
+     * @param xSpeed the new X speed
+     */
     public void setXSpeed(double xSpeed) {this.xSpeed = xSpeed; }
+
+    /**
+     * Sets the speed of the button in the Y direction
+     * @param ySpeed the new Y speed
+     */
     public void setYSpeed(double ySpeed) {this.ySpeed = ySpeed; }
 
 
