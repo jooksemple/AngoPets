740649f6838107ea8d1a11e0d01b1df16aa3e504
Jack Sample <jacks2025@headroyce.org>
2022-05-17 21:28:28 -0700

    Jack Math Game Final Commit JavaFX

--- src/org/headroyce/AngoPets/Main.java
+++ src/org/headroyce/AngoPets/Main.java
@@ -13,47 +13,50 @@ import javafx.stage.Stage;
  * Driver to start the JavaFX program
  */
 public class Main extends Application {
+
     public Main() {
-        }
-
-        /**
-         * Start method where you start on a Settings scene
-         * @param primaryStage
-         * @throws Exception
-         */
-        public void start(Stage primaryStage) throws Exception {
-            GameGUI root = new GameGUI();
-            primaryStage.setTitle("AngoPets");
-            Scene scene = new Scene(root, 600.0D, 600.0D);
-            primaryStage.setScene(scene);
-            scene.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
-                @Override
-                public void handle(KeyEvent keyEvent) {
-                    root.handleKeyPress(keyEvent);
-                }
-            });
-
-            scene.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
-                @Override
-                public void handle(KeyEvent keyEvent) {
-                    root.handleKeyRelease(keyEvent);
-                }
-            });
-            scene.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    root.handleMouseClick(mouseEvent);
-                }
-            });
-            primaryStage.show();
-        }
-        /**
-         * Main method to execute the program
-         */
-        public static void main(String[] args) {
-            launch(args);
-        }
-
-
-
     }
+
+    /**
+     * Start method where you start on a Settings scene
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    public void start(Stage primaryStage) throws Exception {
+        GameGUI root = new GameGUI();
+        primaryStage.setTitle("AngoPets");
+        Scene scene = new Scene(root, 600.0D, 600.0D);
+        primaryStage.setScene(scene);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                root.handleKeyPress(keyEvent);
+            }
+        });
+
+        scene.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                root.handleKeyRelease(keyEvent);
+            }
+        });
+        scene.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                root.handleMouseClick(mouseEvent);
+            }
+        });
+        primaryStage.show();
+    }
+
+    /**
+     * Main method to execute the program
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
+
